31d9369d3de44864003bb2a5c6cf9a6fd02c41c3
LeKaGH <106396832+LeKaGH@users.noreply.github.com>
2023-06-01 09:35:34 +0300

    Update application.js

--- js/application.js
+++ js/application.js
@@ -5,17 +5,17 @@ window.requestAnimationFrame(function () {
 
 
     var imageList = [
-        "https://oispaesaa.tk/img/2.png",
-        "https://oispaesaa.tk/img/4.png",
-        "https://oispaesaa.tk/img/8.png",
-        "https://oispaesaa.tk/img/16.png",
-        "https://oispaesaa.tk/img/32.png",
-        "https://oispaesaa.tk/img/64.png",
-        "https://oispaesaa.tk/img/128.png",
-        "https://oispaesaa.tk/img/256.png",
-        "https://oispaesaa.tk/img/512.png",
-        "https://oispaesaa.tk/img/1024.png",
-        "https://oispaesaa.tk/img/2048.png"
+        "https://lekagh.github.io/Oispa-Esaa/img/2.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/4.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/8.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/16.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/32.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/64.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/128.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/256.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/512.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/1024.png",
+        "https://lekagh.github.io/Oispa-Esaa/img/2048.png"
 
     ];
     for(var i = 0; i < imageList.length; i++ )
